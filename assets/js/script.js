2e17fa319c1b3aa522c7f116ee98995959db5d6a
gurrutia15 <gurrutia15@gmail.com>
2024-04-01 16:16:13 -0300

    formateo de los archivos HTML, CSS y JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-let cambioFondo= ()=>{
-    document.getElementById('boton_form').style.background='#0d6efd'
-    document.getElementById('boton_form').style.color='#ffffff'
+let cambioFondo = () => {
+    document.getElementById('boton_form').style.background = '#0d6efd'
+    document.getElementById('boton_form').style.color = '#ffffff'
 }
 let vueltaColor = () => {
-    document.getElementById('boton_form').style.background='#0dcaf0'
-    document.getElementById('boton_form').style.color='#000000'
+    document.getElementById('boton_form').style.background = '#0dcaf0'
+    document.getElementById('boton_form').style.color = '#000000'
 }
