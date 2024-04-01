b60ee0f5535c713f5102785ae74350ed117346f1
gurrutia15 <gurrutia15@gmail.com>
2024-04-01 15:56:29 -0300

    mouseover y mouseout en botón enviar de formulario

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,14 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+let cambioFondo= ()=>{
+    document.getElementById('boton_form').style.background='#0d6efd'
+    document.getElementById('boton_form').style.color='#ffffff'
+}
+let vueltaColor = () => {
+    document.getElementById('boton_form').style.background='#0dcaf0'
+    document.getElementById('boton_form').style.color='#ffffff'
+}
+
+// #0d6efd
+// #198754
